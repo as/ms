e876d0e45e956068d9d324b1a14df86eae92715c
as <as.utf8@gmail.com>
2017-07-24 10:36:15 -0700

    screen: screenshots

---
+++ screen/example/example.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/as/screen"
+)
+
+func main() {
+	var err error
+	scr, win := 0, 0
+	l := len(os.Args)
+	if l > 1 {
+		scr, err = strconv.Atoi(os.Args[1])
+		no(err)
+	}
+	if l > 2 {
+		win, err = strconv.Atoi(os.Args[2])
+		no(err)
+	}
+
+	fmt.Println(screen.Capture(scr, win, 0, 0, 1024, 768))
+}
+
+func no(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
